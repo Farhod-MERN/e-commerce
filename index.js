ce1ecda7ffec08efcedda16a820118dbef3753ed
Farhod <mrfarhod58@gmail.com>
2023-02-17 22:05:05 +0500

    layouts handlebars

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
 
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname : "hbs" 
+})
+
+app.engine("hbs", hbs.engine)
+app.set("view engine", "hbs")
+app.set("views", "views")
+
+app.get("/",(req, res)=>{
+    res.render("index")
+})
 
 
 const PORT = process.env.PORT || 5000
