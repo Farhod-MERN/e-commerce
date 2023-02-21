38a348e34bbbe0fbbe740e4055138bf9531b77ec
Farhod <mrfarhod58@gmail.com>
2023-02-21 09:45:41 +0500

    Mongoose is Installed

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const homeRouter = require("./routes/home")
-const addRouter = require("./routes/add")
-const productsRouter = require("./routes/products")
-const cardRouter = require("./routes/card")
-const authRouter = require("./routes/auth")
-const app = express()
+const express = require("express");
+const exphbs = require("express-handlebars");
+const homeRouter = require("./routes/home");
+const addRouter = require("./routes/add");
+const productsRouter = require("./routes/products");
+const cardRouter = require("./routes/card");
+const authRouter = require("./routes/auth");
+const app = express();
+const mongoose = require("mongoose");
 
 const hbs = exphbs.create({
-    defaultLayout: "main",
-    extname : "hbs" 
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.engine("hbs", hbs.engine)
-app.set("view engine", "hbs")
-app.set("views", "views")
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/", homeRouter);
+app.use("/add", addRouter);
+app.use("/products", productsRouter);
+app.use("/card", cardRouter);
+app.use("/auth", authRouter);
+app.use(express.json());
 
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
+const starter = async () => {
+  try {
+    const url =
+      "mongodb+srv://farhod:f79cMiYYYphGDQxR@cluster0.ruecq8q.mongodb.net/?retryWrites=true&w=majority";
+      
+      mongoose.set("strictQuery", false);
+      await mongoose.connect(url, {
+      useNewUrlParser: true,
+    });
+    console.log("Mongo is connected");
 
-app.use("/", homeRouter)
-app.use("/add" ,addRouter)
-app.use("/products" ,productsRouter)
-app.use("/card", cardRouter)
-app.use("/auth", authRouter)
-app.use(express.json())
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running : ${PORT}`);
+      console.log(`http://localhost:${PORT}`);
+    });
+  } catch (e){
+    console.log(e);
+  }
+};
 
+starter()
 
+// f79cMiYYYphGDQxR
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server is running : ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-})
+// mongodb+srv://farhod:f79cMiYYYphGDQxR@cluster0.ruecq8q.mongodb.net/?retryWrites=true&w=majority
