69071ee09ef346c4816587ecf1c60d86bc9eff15
Farhod <mrfarhod58@gmail.com>
2023-03-02 15:51:23 +0500

    adding local images is working

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const session = require("express-session")
 const MongoStore = require("connect-mongodb-session")(session)
 const User = require('./models/user')
 const path = require("path")
+const fileUpload = require("express-fileupload")
 const app = express();
 const varMiddleware = require("./middleware/var")
 const userMiddleware = require("./middleware/user")
@@ -38,6 +39,7 @@ app.use(session({
   saveUninitialized:false,
   store: store
 }))
+app.use(fileUpload())
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
--- routes/add.js
+++ routes/add.js
@@ -1,58 +1,76 @@
-const {Router} = require("express")
-const Product = require("../models/product")
-const authMiddleware = require("../middleware/auth")
-const {addVal} = require("../utils/validate")
-const {validationResult} = require("express-validator/check")
+const { Router } = require("express");
+const Product = require("../models/product");
+const authMiddleware = require("../middleware/auth");
+const { addVal } = require("../utils/validate");
+const { validationResult } = require("express-validator/check");
+const path = require("path");
 
-const router = Router()
+const router = Router();
 
-router.get("/", authMiddleware,(req, res)=>{
-    res.render("add",{
-        title: "olx - Create Product",
-        isAdd: true,
-    })
-})
+router.get("/", authMiddleware, (req, res) => {
+  res.render("add", {
+    title: "olx - Create Product",
+    isAdd: true,
+  });
+});
 
-router.post("/",authMiddleware, addVal,async (req, res)=>{
-
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        const {name, quality, tel, description, image, category, address, price} = req.body
-        res.render("add",{
-            title: "olx - Create Product",
-            isAdd: true,
-            data: {name, quality, tel, description, image, category, address, price},
-            error: errors.array()[0].msg
-        })
+router.post("/", authMiddleware, addVal, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const { name, quality, tel, description, image, category, address, price } =
+      await req.body;
+    res.render("add", {
+      title: "olx - Create Product",
+      isAdd: true,
+      data: {
+        name,
+        quality,
+        tel,
+        description,
+        image,
+        category,
+        address,
+        price,
+      },
+      error: errors.array()[0].msg,
+    });
+  }
+  const { image } = await req.files;
+  image.mv(path.resolve(__dirname, "..", "public/posts", image.name), (err) => {
+    if (err) {
+      console.log(err);
     }
-    const product = new Product({
+    Product.create(
+      {
         name: req.body.name,
-        quality:req.body.quality,
+        quality: req.body.quality,
         tel: req.body.tel,
         description: req.body.description,
-        image: req.body.image,
+        image: `/posts/${image.name}`,
         category: req.body.category,
-        address:req.body.address,
+        address: req.body.address,
         price: req.body.price,
-        userId: req.user
-    })
-    try {
-        await product.save()
-        res.redirect("/products") 
+        userId: req.user,
+      },
+      (err, data) => {
+        err ? console.log(err) : console.log(data);
+      }
+    );
+  });
+  try {
+    // await product.save()
+    res.redirect("/products");
+  } catch (error) {
+    console.log(error);
+  }
+  // const data = {...req.body, userId:req.user} // shu orqali postni kim yaratganini bila olamiz
+  // const product = await (await Product.create(data))
+  // console.log(product);
+  // const {name, quality, tel, description, image, category, address, price} = req.body
+  // const product = new Product(name, quality, tel, description, image, category, address, price)
 
-    } catch (error) {
-        console.log(error);
-    }
-    // const data = {...req.body, userId:req.user} // shu orqali postni kim yaratganini bila olamiz
-    // const product = await (await Product.create(data))
-    // console.log(product);
-    // const {name, quality, tel, description, image, category, address, price} = req.body
-    // const product = new Product(name, quality, tel, description, image, category, address, price)
-    
-    // await product.save();
-    // res.redirect("/add")
-})
+  // await product.save();
+  // res.redirect("/add")
+});
 
-
-
-module.exports = router
+module.exports = router;
